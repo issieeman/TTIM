a4162870e6f4812684a9bf69a40b3fd630f57435
issam moussaid <issam.moussaid@hotmail.com>
2017-12-28 12:48:11 +0100

    beginnen met services 28/12/2017 @tijn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,  NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PicnicComponent } from './picnic/picnic.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    PicnicComponent,
+    RestaurantComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([
+      {path: 'home',component: HomeComponent},
+      {path: 'picnic',component: PicnicComponent},
+      {path: 'restaurant',component: RestaurantComponent},
+      {path: 'feedback',component: FeedbackComponent}
+      
+
+    ])
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
